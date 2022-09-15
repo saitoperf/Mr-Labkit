149dad597ed3e3f621136bb03ef577ffcf1e4fe6
saitoperf <saito@perf.cs.tut.ac.jp>
2022-09-15 08:51:14 +0000

    0915

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# ansible-playbook -bK -i inv.ini play-ldap-server.yml
-# ansible-playbook -bK -i inv.ini play-sssd-client.yml
-# ansible-playbook -bK -i inv.ini play-samba-server.yml
-# ansible-playbook -bK -i inv.ini play-prom-server.yml
-# ansible-playbook -bK -i inv.ini play-prom-client.yml
-# ansible-playbook -bK -i inv.ini play-nfs-server.yml
-# ansible-playbook -bK -i inv.ini play-nfs-client.yml
-
 ansible-playbook -bK -i inv.ini \
     play-ldap-server.yml \
     play-sssd-client.yml \
@@ -15,4 +7,12 @@ ansible-playbook -bK -i inv.ini \
     play-prom-server.yml \
     play-prom-client.yml \
     play-nfs-server.yml \
-    play-nfs-client.yml \
+    play-nfs-client.yml
+
+# ansible-playbook -bK -i inv.ini play-ldap-server.yml
+# ansible-playbook -bK -i inv.ini play-sssd-client.yml
+# ansible-playbook -bK -i inv.ini play-samba-server.yml
+# ansible-playbook -bK -i inv.ini play-prom-server.yml
+# ansible-playbook -bK -i inv.ini play-prom-client.yml
+# ansible-playbook -bK -i inv.ini play-nfs-server.yml
+# ansible-playbook -bK -i inv.ini play-nfs-client.yml
