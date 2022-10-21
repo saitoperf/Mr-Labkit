a0d6a1815bef2455f2c08ae93ca1b9b67e9b4fd3
saitoperf <saito@perf.cs.tut.ac.jp>
2022-10-21 00:49:31 +0000

    edit run.sh

--- src/run.sh
+++ src/run.sh
@@ -23,7 +23,11 @@ elif [ "$1" = 'nfs-client' ]; then
     ansible-playbook -bK -i inv.ini play-nfs-client.yml
     sed -i -e "s/\/c[1-99]\//\/s1\//g" inv.ini
 elif [ "$1" = 'gitlab' ]; then
-    ansible-playbook -bK -i inv.ini play-gitlab-server.yml
+    if [ "$2" = "get-pass" ]; then
+        vagrant ssh s1 -- docker exec gitlab grep 'Password:' /etc/gitlab/initial_root_password
+    else
+        ansible-playbook -bK -i inv.ini play-gitlab-server.yml
+    fi
 elif [ "$1" = 'all' ]; then
     ansible-playbook -bK -i inv.ini \
         play-ldap-server.yml \
@@ -35,11 +39,19 @@ elif [ "$1" = 'all' ]; then
         play-nfs-server.yml \
         play-nfs-client.yml
     sed -i -e "s/\/c[1-99]\//\/s1\//g" inv.ini
+
 else
     echo 'Usage: ./run.sh [OPTION]'
     echo 'options'
-    echo '    generate: Run File generator'
-    echo '    vagrant : Provision VM'
-    echo '    all     : Install all services'
-    echo '    ldap, sssd, samba, prom-server, prom-client, nfs-server, nfs-client, gitlab'
+    echo '    generate      : Run File generator'
+    echo '    vagrant       : Provision VM'
+    echo '    all           : Install all services'
+    echo '    ldap          :'
+    echo '    sssd          :'
+    echo '    samba         :'
+    echo '    prom-server   :'
+    echo '    prom-client   :'
+    echo '    nfs-server    :'
+    echo '    nfs-client    :'
+    echo '    gitlab        :get-pass'
 fi
